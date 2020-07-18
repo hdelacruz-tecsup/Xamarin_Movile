e2e01fe0bc5f6ab3c008c9928db02fdfc0470b9e
hugo <hugo.delacruz@tecsup.edu.pe>
2020-07-17 20:04:01 -0500

    Comit Final

---
+++ TodoREST/Constants.cs
@@ -0,0 +1,10 @@
+﻿using Xamarin.Forms;
+
+namespace TodoREST
+{
+    public static class Constants
+    {
+        // URL of REST service
+        public static string RestUrl = "https://noderestful16.herokuapp.com/personal1";
+    }
+}
---
+++ TodoREST/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TodoREST
+{
+	public class TodoItem
+	{
+		public string id { get; set; }
+
+		public string nombreApp { get; set; }
+
+		public string apellido { get; set; }
+		public string sexo { get; set; }
+
+		public string edad { get; set; }
+
+		public string FullName => $"{nombreApp} {apellido}";
+	}
+}
